ad6f2e9c02147c8d0292e41887df67e4ba8d90a7
Paul Rathbone <socentral2@hotmail.co.uk>
2023-11-21 21:54:02 +0000

    drop down logic added

--- resources/ntp.js
+++ resources/ntp.js
@@ -1,11 +1,30 @@
-var myArray = new Array("1", "2", "3", "4", "5");
+//https://stackoverflow.com/questions/9895082/javascript-populate-drop-down-list-with-array
 
+var selectHours = document.getElementById("selectHours");
+var options = ["-13", "-12", "-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"];
 
-// Get dropdown element from DOM
-var dropdown = document.getElementById("selectHours");
+for(var i = 0; i < options.length; i++) {
+    var opt = options[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectHours.appendChild(el);
+}
 
-// Loop through the array
-for (var i = 0; i < myArray.length; ++i) {
-    // Append the element to the end of Array list
-    dropdown[dropdown.length] = new Option(myArray[i], myArray[i]);
+var selectMinutes = document.getElementById("selectMinutes");
+for(var i = 0; i < 61; i++) {
+    var opt = i;
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectMinutes.appendChild(el);
+}
+
+var selectSeconds = document.getElementById("selectSeconds");
+for(var i = 0; i < 61; i++) {
+    var opt = i;
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectSeconds.appendChild(el);
 }
